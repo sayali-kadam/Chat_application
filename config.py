d220e51444ab3113db326f8e74586c8df600193c
sayali-kadam <62012906+sayali-kadam@users.noreply.github.com>
2023-01-02 20:26:20 +0530

    First commit

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Sayali@13@/customer-service-messaging'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
-    SECRET_KEY='supersecret'
